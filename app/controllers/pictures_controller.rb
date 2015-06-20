24dfd688266195cc4e977fa5d2b817773f47e99a
Stefan Lissia <stefan.emailacct@gmail.com>
2015-06-20 21:49:47 +0100

    6 tests passed up to show picture

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -4,4 +4,22 @@ class PicturesController < ApplicationController
     @pictures = Picture.all
   end
 
+  def new
+    @picture = Picture.new
+  end
+
+  def show
+    @picture = Picture.find(params[:id])
+  end
+
+  def create
+    @picture = Picture.create(picture_params)
+    redirect_to '/pictures'
+  end
+
+  def picture_params
+    params.require(:picture).permit(:name)
+  end
+
+
 end
--- spec/features/pictures_feature_spec.rb
+++ spec/features/pictures_feature_spec.rb
@@ -20,5 +20,28 @@ feature 'pictures' do
     end
   end
 
+  context 'creating a picture' do
+    scenario 'prompts user to fill out form then displays the picture' do
+        visit '/pictures'
+        click_link 'Add a picture'
+        fill_in 'Name', with: 'pic1'
+        click_button 'Create Picture'
+        expect(page).to have_content 'pic1'
+        expect(current_path).to eq '/pictures'
+    end
+  end
+
+  context 'viewing pictures' do
+    let!(:pic1){Picture.create(name:'pic1')}
+
+    scenario 'user can view picture' do
+        visit '/pictures'
+        click_link 'pic1'
+        expect(page).to have_content 'pic1'
+        expect(current_path).to eq "/pictures/#{pic1.id}"
+
+    end
+  end
+
 
 end
